84b227c1d02ac987bb2e5cc87599bac0c885d374
Rajath Kiran A <arajath5463@gmail.com>
2026-07-03 22:38:41 +0530

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int result_string=0;
+        for(int i=0;i<s.length()-1;i++){
+            result_string+=abs(s[i]-s[i+1]);
+        }
+
+        return result_string;
+    }
+};
